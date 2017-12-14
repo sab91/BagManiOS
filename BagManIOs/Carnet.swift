6630671649261cd6e90d65882bfe96ed310ac99f
if26 <if26@mac01-m102.utt.fr>
2017-12-14 19:02:04 +0100

    Initialisation du projet Xcode

---
+++ BagManIOs/Carnet.swift
@@ -0,0 +1,9 @@
+//
+//  Carnet.swift
+//  BagManIOs
+//
+//  Created by if26 on 14/12/2017.
+//  Copyright © 2017 utt. All rights reserved.
+//
+
+import Foundation
---
+++ BagManIOs/Page.swift
@@ -0,0 +1,9 @@
+//
+//  Page.swift
+//  BagManIOs
+//
+//  Created by if26 on 14/12/2017.
+//  Copyright © 2017 utt. All rights reserved.
+//
+
+import Foundation
