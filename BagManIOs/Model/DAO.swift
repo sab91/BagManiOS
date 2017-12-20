3a00e40b83b80d82ef52ae6167ccaf1c8b7d7ce9
sab91 <sabride@hotmail.fr>
2017-12-20 16:12:22 +0100

    Ajout DAO

---
+++ BagManIOs/Model/DAO.swift
@@ -0,0 +1,43 @@
+//
+//  DAO.swift
+//  BagManIOs
+//
+//  Created by Sabri on 20/12/2017.
+//  Copyright © 2017 utt. All rights reserved.
+//
+
+import Foundation
+import SQLite
+
+class DAO {
+    
+//    static let NAME = Expression<String>("name")
+//    static let TITLE = Expression<String>("title")
+//    static let CONTENT = Expression<String>("content")
+//    static let SUMMARY = Expression<String>("summary")
+//    static let CREATED_AT = Expression<Date>("created_at")
+//    static let UPDATED_AT = Expression<Date>("updated_at")
+//    static let ID = Expression<Int>("id")
+    
+    static func objectToPage(cursor: Row) -> Page {
+         let NAME = Expression<String>("name")
+         let TITLE = Expression<String>("title")
+         let CONTENT = Expression<String>("content")
+         let SUMMARY = Expression<String>("summary")
+         let CREATED_AT = Expression<Date>("created_at")
+         let UPDATED_AT = Expression<Date>("updated_at")
+         let ID = Expression<Int>("id")
+        
+        return Page(id_pf: cursor[ID], title_pf: cursor[TITLE], content_pf: cursor[CONTENT], summary_pf: cursor[SUMMARY], createdAt_pf: cursor[CREATED_AT], updatedAt_pf: cursor[UPDATED_AT])
+    }
+    
+    static func objectToCarnet(cursor: Row) -> Carnet {
+        let NAME = Expression<String>("name")
+        let CREATED_AT = Expression<Date>("created_at")
+        let UPDATED_AT = Expression<Date>("updated_at")
+        let ID = Expression<Int>("id")
+        
+        return Carnet(id_pf: cursor[ID], name_pf: cursor[NAME], createdAt_cf: cursor[CREATED_AT], updatedAt_cf: cursor[UPDATED_AT])
+    }
+    
+}
